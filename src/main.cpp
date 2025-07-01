5d017da77e29c27777cb6041522d0a8f942eb60c
VentiFBT1 <Jesusrsantos18@gmail.com>
2025-07-01 16:58:30 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -69,7 +69,7 @@ void setup() {
 	logger.info("SlimeVR v" FIRMWARE_VERSION " starting up...");
 
 	led.begin();
-	led.setPixelColor(0, led.Color(15, 15, 15));
+	led.setPixelColor(0, led.Color(33, 33, 33));
 	led.show();
 
 	statusManager.setStatus(SlimeVR::Status::LOADING, true);
